d51aed96e8ac47fba3afad746c1bb1b9498db745
KevinYang-github <yangkaibo0815@gmail.com>
2024-06-03 00:30:48 +1000

    feat: #414 update user information

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -4,13 +4,14 @@ const validateParams = require('../middlewares/validateMiddleware');
 const asyncHandler = require('../utils/asyncHandler');
 
 const { createUserValidatorSchema } = require('../validators/userValidator');
+const { updateUserValidatorSchema } = require('../validators/userValidator');
 
 const router = express.Router();
 
 router.get('/api/v1/users', asyncHandler(UserController.getAllUsers));
 router.post('/api/v1/users', validateParams(createUserValidatorSchema), asyncHandler(UserController.createUser));
 router.get('/api/v1/users/:id', asyncHandler(UserController.getUserById));
-// router.put('/users/:id', UserController.updateUser);
+router.patch('/api/v1/users/:id', validateParams(updateUserValidatorSchema), asyncHandler(UserController.updateUser));
 // router.delete('/users/:id', UserController.deleteUser);
 
 module.exports = router;
