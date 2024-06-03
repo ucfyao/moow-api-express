48a6e07f0f95cee0853edede1572985999806e3b
wjtwzgl12 <jintaowang2022@163.com>
2024-06-03 15:41:33 +1000

    feat: #609 exchange key api

--- app/controllers/keyController.js
+++ app/controllers/keyController.js
@@ -1,9 +1,8 @@
 // app/controllers/KeyController.js
 const KeyService = require('../services/keyService');
 const ResponseHandler = require('../utils/responseHandler');
-const { STATUS_TYPE } = require('../utils/statusCodes');
-const { validateBinanceKeys } = require('../validators/binanceValidator');
-const { decrypt } = require('../utils/cryptoUtils');
+const { STATUS_TYPE } = require('../constants/statusCodes');
+
 
 class KeyController {
 
@@ -17,62 +16,24 @@ class KeyController {
   //   res.success(res, newUser, STATUS_TYPE.http.created, STATUS_TYPE.success);
   // };
   async getAllKeys(req, res) {
-    try {
-      const keys = await KeyService.getAllKeys();
-      ResponseHandler.success(res, keys);
-    } catch (error) {
-      ResponseHandler.fail(res, STATUS_TYPE.internalServerError, STATUS_TYPE.internalError, error.message);
-    }
+    const keys = await KeyService.getAllKeys();
+    return ResponseHandler.success(res, keys);
   }
 
   async getKeyById(req, res) {
-    try {
-      const key = await KeyService.getKeyById(req.params.id);
-      if (key) {
-          ResponseHandler.success(res, key);
-      } else {
-          ResponseHandler.fail(res, STATUS_TYPE.notFound, STATUS_TYPE.internalError, 'Key not found');
-        }
-      } catch (error) {
-        ResponseHandler.fail(res, STATUS_TYPE.internalServerError, STATUS_TYPE.internalError, error.message);
-      }
-    }
-
+    const key = await KeyService.getKeyById(req.params.id);
+    return ResponseHandler.success(res, key);
+  }
   async createKey(req, res) {
-    try {
-      const { exchange, access_key, secret_key, desc } = req.body;
-      const keyData = { exchange, access_key, secret_key, desc };
-      console.log(keyData); // Debug log
-      const validation = await validateBinanceKeys(access_key, secret_key);
-      if (!validation.valid) {
-        return ResponseHandler.fail(res, STATUS_TYPE.badRequest, STATUS_TYPE.paramsError, validation.error);
-      }
-      const key = await KeyService.createKey( keyData );
-      // Partially intercept the returned API key and secret
-      const decryptedaccess_key = decrypt(JSON.parse(key.access_key));
-      const decryptedsecret_key = decrypt(JSON.parse(key.secret_key));
-      key.access_key = `${decryptedaccess_key.slice(0, 3)}******${decryptedaccess_key.slice(-4)}`;
-      key.secret_key = `${decryptedsecret_key.slice(0, 3)}******${decryptedsecret_key.slice(-4)}`;
+    const { exchange, access_key, secret_key, desc } = req.body;
+    const keyData = { exchange, access_key, secret_key, desc };
 
-      ResponseHandler.success(res, { key, balances: validation.data }, STATUS_TYPE.created);
-    } catch (error) {
-      console.error("Controller error:", error); // Debug log
-      ResponseHandler.fail(res, STATUS_TYPE.internalServerError, STATUS_TYPE.internalError, error.message);
-    }
+    const { key, validation } = await KeyService.createKey(keyData);
+    return ResponseHandler.success(res, { key, validation }, STATUS_TYPE.created);
   }
-
   async deleteKey(req, res) {
-    try {
-      const result = await KeyService.deleteKey(req.params.id);
-      if (result) {
-        ResponseHandler.success(res, { message: 'Key deleted successfully' });
-      } else {
-        ResponseHandler.fail(res, STATUS_TYPE.notFound, STATUS_TYPE.internalError, 'Key not found');
-      }
-    } catch (error) {
-      ResponseHandler.fail(res, STATUS_TYPE.internalServerError, STATUS_TYPE.internalError, error.message);
-    }
+    const result = await KeyService.deleteKey(req.params.id);
+    return ResponseHandler.success(res, { message: 'Key deleted successfully' });
   }
-
 }
 module.exports = new KeyController();
--- app/models/keyModel.js
+++ app/models/keyModel.js
@@ -1,6 +1,5 @@
 // models/keyModel.js
 const mongoose = require('mongoose');
-const { encrypt, decrypt } = require('../utils/cryptoUtils');
 
 const KeySchema = new mongoose.Schema({
   uid: { type: mongoose.Schema.Types.ObjectId }, // User Id, ref: 'PortalUser'
@@ -12,23 +11,9 @@ const KeySchema = new mongoose.Schema({
   is_deleted: { type: Boolean, default: false }, // delete mark
 }, {
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
-  collection: 'aip_key',
+  collection: 'key',
 });
-
-KeySchema.pre('save', function (next) {
-  if (this.isNew) {
-    // Encrypt access_key and secret_key
-    this.access_key = JSON.stringify(encrypt(this.access_key));
-    this.secret_key = JSON.stringify(encrypt(this.secret_key));
-
-    //Generate desensitized secret_show
-    const decryptedsecret_key = decrypt(JSON.parse(this.secret_key));
-    this.secret_show = `${decryptedsecret_key.slice(0, 3)}******${decryptedsecret_key.slice(-3)}`;
-  }
-  next();
-});
-
-//KeySchema.index({ _id: 1 }, { unique: true });
 
 const Key = mongoose.model('Key', KeySchema);
+
 module.exports = Key;
