74be41edcb0a9d25dd4e4ccca5b41f824b3f040d
wjtwzgl12 <jintaowang2022@163.com>
2024-06-03 15:58:38 +1000

    feat: #609 exchange key api

--- app/routes/keyRoutes.js
+++ app/routes/keyRoutes.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const KeyController = require('../controllers/keyController');
 const validateParams = require('../middlewares/validateMiddleware');
-
+const asyncHandler = require('../utils/asyncHandler');
 const { createKeyValidatorSchema } = require('../validators/keyValidator');
 
 const router = express.Router();
 
-router.get('/keys', KeyController.getAllKeys);
-router.post('/keys', validateParams(createKeyValidatorSchema), KeyController.createKey);
-router.get('/keys/:id', KeyController.getKeyById);
+router.get('/keys', asyncHandler(KeyController.getAllKeys));
+router.post('/keys', validateParams(createKeyValidatorSchema), asyncHandler(KeyController.createKey));
+router.get('/keys/:id', asyncHandler(KeyController.getKeyById));
 
 
 module.exports = router;
