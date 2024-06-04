1719a331d1e3bb5c3cf44501e6a75a94db22b4eb
KevinYang-github <yangkaibo0815@gmail.com>
2024-06-04 12:15:18 +1000

    feat: #411 user reset password

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -3,11 +3,12 @@ const AuthController = require("../controllers/authController");
 const validateParams = require("../middlewares/validateMiddleware");
 const { signinValidatorSchema, exitValidatorSchema } = require("../validators/authValidator");
 const authMiddleware = require("../middlewares/authMiddleware");
+const asyncHandler = require('../utils/asyncHandler');
 
 const router = express.Router();
 
 router.get("/captcha", AuthController.getCaptcha);
 router.post("/auth/login", validateParams(signinValidatorSchema), AuthController.signin);
 router.delete("/auth/logout", validateParams(exitValidatorSchema), authMiddleware, AuthController.exit);
-
+router.patch("/auth/resetPassword", asyncHandler(AuthController.resetPassword))
 module.exports = router;
